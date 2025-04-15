bafda51c091e9826813e4f3a3260133882271399
xvntr <164462454+xvntr@users.noreply.github.com>
2025-04-15 20:37:48 +0700

    remove express.js

--- routes/articleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllArticles, createArticle } = require('../controllers/articleController');
-
-router.get('/', getAllArticles);
-router.post('/', createArticle);
-
-module.exports = router;
