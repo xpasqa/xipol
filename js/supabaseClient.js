a9453f0cfde7c977283cbbddf16f2774e688feb0
xvntr <164462454+xvntr@users.noreply.github.com>
2025-05-17 22:51:19 +0700

    update js keys

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,6 @@
-// js/supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 export const supabase = createClient(
-  'https://YOUR_PROJECT_URL.supabase.co',
-  'YOUR_ANON_KEY'
+  'https://zpniygdnkrrgllgxfjiv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwbml5Z2Rua3JyZ2xsZ3hmaml2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0OTQ4NDQsImV4cCI6MjA2MzA3MDg0NH0.zRa40N2t-uENazXYk46O0WJoOhAzk5McKmapofv5Lpk'
 )
